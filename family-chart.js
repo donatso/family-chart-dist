dd91222ce09ecaa374687068d4d6fcf7497a637d
donatso <soricdonat@gmail.com>
2021-05-31 15:33:35 -0500

    checkpoint

--- family-chart.js
+++ family-chart.js
@@ -202,6 +202,100 @@ function generateUUID() {
   });
 }
 
+function handleRelsOfNewDatum({datum, data_stash, rel_type, rel_datum}) {
+  if (rel_type === "daughter" || rel_type === "son") addChild(datum);
+  else if (rel_type === "father" || rel_type === "mother") addParent(datum);
+  else if (rel_type === "spouse") addSpouse(datum);
+
+  function addChild(datum) {
+    if (datum.data.other_parent) {
+      addChildToSpouseAndParentToChild(datum.data.other_parent);
+      delete datum.data.other_parent;
+    }
+    datum.rels[rel_datum.data.gender === 'M' ? 'father' : 'mother'] = rel_datum.id;
+    if (!rel_datum.rels.children) rel_datum.rels.children = [];
+    rel_datum.rels.children.push(datum.id);
+    return datum
+
+    function addChildToSpouseAndParentToChild(spouse_id) {
+      if (spouse_id === "_new") spouse_id = addOtherParent().id;
+
+      const spouse = data_stash.find(d => d.id === spouse_id);
+      datum.rels[spouse.data.gender === 'M' ? 'father' : 'mother'] = spouse.id;
+      if (!spouse.rels.hasOwnProperty('children')) spouse.rels.children = [];
+      spouse.rels.children.push(datum.id);
+
+      function addOtherParent() {
+        const new_spouse = createNewPersonWithGenderFromRel({rel_type: "spouse", rel_datum});
+        addSpouse(new_spouse);
+        addNewPerson({data_stash, datum: new_spouse});
+        return new_spouse
+      }
+    }
+  }
+
+  function addParent(datum) {
+    const is_father = datum.data.gender === "M",
+      parent_to_add_id = rel_datum.rels[is_father ? 'father' : 'mother'];
+    if (parent_to_add_id) removeToAdd(data_stash.find(d => d.id === parent_to_add_id), data_stash);
+    addNewParent();
+
+    function addNewParent() {
+      rel_datum.rels[is_father ? 'father' : 'mother'] = datum.id;
+      handleSpouse();
+      datum.rels.children = [rel_datum.id];
+      return datum
+
+      function handleSpouse() {
+        const spouse_id = rel_datum.rels[!is_father ? 'father' : 'mother'];
+        if (!spouse_id) return
+        const spouse = data_stash.find(d => d.id === spouse_id);
+        datum.rels.spouses = [spouse_id];
+        if (!spouse.rels.spouses) spouse.rels.spouses = [];
+        spouse.rels.spouses.push(datum.id);
+        return spouse
+      }
+    }
+  }
+
+  function addSpouse(datum) {
+    removeIfToAdd();
+    if (!rel_datum.rels.spouses) rel_datum.rels.spouses = [];
+    rel_datum.rels.spouses.push(datum.id);
+    datum.rels.spouses = [rel_datum.id];
+
+    function removeIfToAdd() {
+      if (!rel_datum.rels.spouses) return
+      rel_datum.rels.spouses.forEach(spouse_id => {
+        const spouse = data_stash.find(d => d.id === spouse_id);
+        if (spouse.to_add) removeToAdd(spouse, data_stash);
+      });
+    }
+  }
+}
+
+function createNewPerson({data, rels}) {
+  return {id: generateUUID(), data: data || {}, rels: rels || {}}
+}
+
+function createNewPersonWithGenderFromRel({data, rel_type, rel_datum}) {
+  const gender = getGenderFromRelative(rel_datum, rel_type);
+  data = Object.assign(data || {}, {gender});
+  return createNewPerson({data})
+
+  function getGenderFromRelative(rel_datum, rel_type) {
+    return (["daughter", "mother"].includes(rel_type) || rel_type === "spouse" && rel_datum.data.gender === "M") ? "F" : "M"
+  }
+}
+
+function addNewPerson({data_stash, datum}) {
+  data_stash.push(datum);
+}
+
+function createTreeDataWithMainNode({data}) {
+  return {data: [createNewPerson({data})], version: null}
+}
+
 function CalculateTree({data_stash, main_id=null, is_vertical=true, node_separation=250, level_separation=150}) {
   data_stash = createRelsToAdd(data_stash);
   sortChildrenWithSpouses(data_stash);
@@ -357,8 +451,11 @@ function CalculateTree({data_stash, main_id=null, is_vertical=true, node_separat
     return data
 
     function createToAddSpouse(d) {
-      const spouse = {id: generateUUID(), rels: {spouses: [d.id], children: []},
-        data: {gender: d.data.gender === "M" ? "F" : "M"}, to_add: true};
+      const spouse = createNewPerson({
+        data: {gender: d.data.gender === "M" ? "F" : "M"},
+        rels: {spouses: [d.id], children: []}
+      });
+      spouse.to_add = true;
       to_add_spouses.push(spouse);
       return spouse
     }
@@ -751,104 +848,6 @@ function CalculateTree$1({datum, data_stash, card_dim}) {
   return {data}
 }
 
-function handleRelsOfNewDatum({datum, data_stash, rel_type, rel_datum}) {
-  if (rel_type === "daughter" || rel_type === "son") addChild(datum);
-  else if (rel_type === "father" || rel_type === "mother") addParent(datum);
-  else if (rel_type === "spouse") addSpouse(datum);
-
-  function addChild(datum) {
-    if (datum.data.other_parent) {
-      addChildToSpouseAndParentToChild(datum.data.other_parent);
-      delete datum.data.other_parent;
-    }
-    datum.rels[rel_datum.data.gender === 'M' ? 'father' : 'mother'] = rel_datum.id;
-    if (!rel_datum.rels.children) rel_datum.rels.children = [];
-    rel_datum.rels.children.push(datum.id);
-    return datum
-
-    function addChildToSpouseAndParentToChild(spouse_id) {
-      if (spouse_id === "_new") spouse_id = addOtherParent().id;
-
-      const spouse = data_stash.find(d => d.id === spouse_id);
-      datum.rels[spouse.data.gender === 'M' ? 'father' : 'mother'] = spouse.id;
-      if (!spouse.rels.hasOwnProperty('children')) spouse.rels.children = [];
-      spouse.rels.children.push(datum.id);
-
-      function addOtherParent() {
-        const new_spouse = createNewPersonWithGenderFromRel({rel_type: "spouse", rel_datum});
-        addSpouse(new_spouse);
-        addNewPerson({data_stash, datum: new_spouse});
-        console.log(data_stash);
-        return new_spouse
-      }
-    }
-  }
-
-  function addParent(datum) {
-    const is_father = datum.data.gender === "M",
-      parent_to_add_id = rel_datum.rels[is_father ? 'father' : 'mother'];
-    if (parent_to_add_id) removeToAdd(data_stash.find(d => d.id === parent_to_add_id), data_stash);
-    addNewParent();
-
-    function addNewParent() {
-      rel_datum.rels[is_father ? 'father' : 'mother'] = datum.id;
-      handleSpouse();
-      datum.rels.children = [rel_datum.id];
-      return datum
-
-      function handleSpouse() {
-        const spouse_id = rel_datum.rels[!is_father ? 'father' : 'mother'];
-        if (!spouse_id) return
-        const spouse = data_stash.find(d => d.id === spouse_id);
-        datum.rels.spouses = [spouse_id];
-        if (!spouse.rels.spouses) spouse.rels.spouses = [];
-        spouse.rels.spouses.push(datum.id);
-        return spouse
-      }
-    }
-  }
-
-  function addSpouse(datum) {
-    removeIfToAdd();
-    if (!rel_datum.rels.spouses) rel_datum.rels.spouses = [];
-    rel_datum.rels.spouses.push(datum.id);
-    datum.rels.spouses = [rel_datum.id];
-
-    function removeIfToAdd() {
-      if (!rel_datum.rels.spouses) return
-      rel_datum.rels.spouses.forEach(spouse_id => {
-        const spouse = data_stash.find(d => d.id === spouse_id);
-        if (spouse.to_add) removeToAdd(spouse, data_stash);
-      });
-    }
-  }
-}
-
-function createNewPerson({data}) {
-  return {id: generateUUID(), data: data || {}, rels: {}}
-}
-
-function createNewPersonWithGenderFromRel({data, rel_type, rel_datum}) {
-  const gender = getGenderFromRelative(rel_datum, rel_type);
-  data = Object.assign(data || {}, {gender});
-  return createNewPerson({data})
-
-  function getGenderFromRelative(rel_datum, rel_type) {
-    return (["daughter", "mother"].includes(rel_type) || rel_type === "spouse" && rel_datum.data.gender === "M") ? "F" : "M"
-  }
-}
-
-
-function newRelative({data_stash, rel_type, rel_datum}) {
-  const rel_data = {gender: getGenderFromRelative(rel_datum, rel_type)},
-    datum = createNewPerson({data: rel_data});
-  return {new_rel: datum, addNewRel}
-}
-
-function addNewPerson({data_stash, datum}) {
-  data_stash.push(datum);
-}
-
 function View(store, tree, datum) {
   const data_stash = store.getData(),
     svg_dim = store.state.cont.getBoundingClientRect(),
@@ -1030,8 +1029,8 @@ cardShowHideRels: cardShowHideRels,
 handleRelsOfNewDatum: handleRelsOfNewDatum,
 createNewPerson: createNewPerson,
 createNewPersonWithGenderFromRel: createNewPersonWithGenderFromRel,
-newRelative: newRelative,
 addNewPerson: addNewPerson,
+createTreeDataWithMainNode: createTreeDataWithMainNode,
 checkIfRelativesConnectedWithoutPerson: checkIfRelativesConnectedWithoutPerson
 });
 
@@ -1220,7 +1219,7 @@ function createStore(initial_state) {
     setOnUpdate = (f) => onUpdate = f,
     methods = {};
 
-  if (!state.data || state.data.length === 0) state.data = [{to_add: true, rels: {}, data: {}, id: generateUUID()}];
+  if (!state.data || state.data.length === 0) state.data = createTreeDataWithMainNode().data;
 
   return {state, update, getData, getTree, setOnUpdate, methods}
 
